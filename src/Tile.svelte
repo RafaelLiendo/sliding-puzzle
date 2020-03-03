53bed41d766d1c22ab3ee2577815d2678297dc0f
Rafael Liendo <rafael.n.liendo@gmail.com>
2020-03-03 17:56:30 -0300

    WIP

---
+++ src/Tile.svelte
@@ -0,0 +1,11 @@
+<script>
+export let col;
+export let row;
+export let colStart;
+export let rowStart;
+
+$: element.style.setProperty('--row', row);
+$: element.style.setProperty('--col', col);
+$: element.style.setProperty('--col-start', colStart);
+$: element.style.setProperty('--col-start', rowStart);
+</script>
